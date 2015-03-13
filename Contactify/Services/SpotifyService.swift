5e72fe35cc5dd961108623f985d42793caf0f8d5
Tony Brock <abrock17@gmail.com>
2015-03-13 00:28:05 -0400

    Finally some functionality calling to authenticate with Spotify

--- Contactify/Services/SpotifyService.swift
+++ Contactify/Services/SpotifyService.swift
@@ -1,9 +1,40 @@
-//
-//  SpotifyService.swift
-//  Contactify
-//
-//  Created by Tony Brock on 3/8/15.
-//  Copyright (c) 2015 Tony Brock. All rights reserved.
-//
-
 import Foundation
+
+public class SpotifyService {
+    
+    let clientID = "02b72a9ba42742acbebb0d3277c9996f"
+    let callbackURL = "name-playlist-creator-login://return"
+    let sessionDataKey = "SpotifySessionData"
+    let uiApplicationWrapper: UIApplicationWrapper!
+    
+    public init(uiApplicationWrapper: UIApplicationWrapper = UIApplicationWrapper()) {
+        self.uiApplicationWrapper = uiApplicationWrapper
+    }
+    
+    public func getSession() -> SPTSession? {
+        var session: SPTSession?
+        if let sessionData = NSUserDefaults.standardUserDefaults().objectForKey(sessionDataKey) as? NSData {
+            session = NSKeyedUnarchiver.unarchiveObjectWithData(sessionData) as? SPTSession
+        } else {
+            let auth = SPTAuth.defaultInstance()
+            let loginURL = auth.loginURLForClientId(clientID, declaredRedirectURL: NSURL(string: callbackURL), scopes: [SPTAuthPlaylistModifyPublicScope, SPTAuthPlaylistModifyPrivateScope, SPTAuthStreamingScope])
+            dispatch_after(
+                dispatch_time(DISPATCH_TIME_NOW, Int64(0.1 * Double(NSEC_PER_SEC))),
+                dispatch_get_main_queue()) {
+                    self.uiApplicationWrapper.openURL(loginURL)
+                    return
+            }
+        }
+        return session
+    }
+}
+
+public class UIApplicationWrapper {
+    
+    public init() {
+    }
+    
+    public func openURL(url: NSURL) -> Bool {
+        return UIApplication.sharedApplication().openURL(url)
+    }
+}
--- ContactifyTests/Services/SpotifyServiceSpec.swift
+++ ContactifyTests/Services/SpotifyServiceSpec.swift
@@ -1,9 +1,56 @@
-//
-//  SpotifyServiceSpec.swift
-//  Contactify
-//
-//  Created by Tony Brock on 3/8/15.
-//  Copyright (c) 2015 Tony Brock. All rights reserved.
-//
-
+import Contactify
 import Foundation
+import Quick
+import Nimble
+
+class SpotifyServiceSpec: QuickSpec {
+    let sessionDataKey = "SpotifySessionData"
+    
+    override func spec() {
+        
+        describe("The SpotifyService") {
+            var spotifyService: SpotifyService!
+            var mockUIApplicationWrapper: MockUIApplicationWrapper!
+            
+            
+            beforeEach() {
+                mockUIApplicationWrapper = MockUIApplicationWrapper()
+                spotifyService = SpotifyService(uiApplicationWrapper: mockUIApplicationWrapper)
+            }
+            
+            afterEach() {
+                NSUserDefaults.standardUserDefaults().removeObjectForKey(self.sessionDataKey)
+            }
+            
+            describe("get session") {
+                it("retrieves the session from user defaults") {
+                    let expectedSession = SPTSession(userName: "user", accessToken: "token", expirationDate: NSDate())
+                    let sessionData = NSKeyedArchiver.archivedDataWithRootObject(expectedSession)
+                    NSUserDefaults.standardUserDefaults().setObject(sessionData, forKey: self.sessionDataKey)
+                    
+                    let actualSession = spotifyService.getSession()
+                    expect(actualSession).toNot(beNil())
+                    expect(actualSession?.accessToken).to(equal(expectedSession.accessToken))
+                }
+                
+                context("when the session is not in user defaults") {
+                    it("opens the login URL") {
+                        let actualSession = spotifyService.getSession()
+                        expect(mockUIApplicationWrapper.capturedUrl).toEventuallyNot(beNil())
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+class MockUIApplicationWrapper: UIApplicationWrapper {
+    
+    var capturedUrl: NSURL?
+    
+    override func openURL(url: NSURL) -> Bool {
+        self.capturedUrl = url
+        return true
+    }
+}
