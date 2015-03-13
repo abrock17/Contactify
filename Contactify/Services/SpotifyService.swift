92e78973a8285267d1a36d7a653d4421e11049b9
Tony Brock <abrock17@gmail.com>
2015-03-13 00:27:40 -0400

    Finally some functionality calling to authenticate with Spotify

---
+++ Contactify/Services/SpotifyService.swift
@@ -0,0 +1,9 @@
+//
+//  SpotifyService.swift
+//  Contactify
+//
+//  Created by Tony Brock on 3/8/15.
+//  Copyright (c) 2015 Tony Brock. All rights reserved.
+//
+
+import Foundation
---
+++ ContactifyTests/Services/SpotifyServiceSpec.swift
@@ -0,0 +1,9 @@
+//
+//  SpotifyServiceSpec.swift
+//  Contactify
+//
+//  Created by Tony Brock on 3/8/15.
+//  Copyright (c) 2015 Tony Brock. All rights reserved.
+//
+
+import Foundation
