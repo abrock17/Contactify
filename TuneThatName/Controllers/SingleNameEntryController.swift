b361fb5c5995dc6548658d0df9b033e5e5dce04e
Tony Brock <abrock17@gmail.com>
2015-10-01 17:59:48 -0400

    test-drove the name entry page - all is right with the world

--- TuneThatName/Controllers/SingleNameEntryController.swift
+++ TuneThatName/Controllers/SingleNameEntryController.swift
@@ -1,20 +1,25 @@
 import UIKit
 
-public class SingleNameEntryController: UIViewController {
+public class SingleNameEntryController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    public var contact = Contact(id: -1, firstName: nil, lastName: nil)
+    public var selectedContact: Contact?
     var allConctacts = [Contact]()
+    var suggestedContacts = [Contact]()
     public var songSelectionCompletionHandler: ((Song, Contact?) -> Void)!
     
     public var contactService = ContactService()
     
     @IBOutlet public weak var nameEntryTextField: UITextField!
+    @IBOutlet public weak var lastNameLabel: UILabel!
+    @IBOutlet public weak var nameSuggestionTableView: UITableView!
     @IBOutlet public weak var doneButton: UIBarButtonItem!
     
     override public func viewDidLoad() {
         super.viewDidLoad()
 
         nameEntryTextChanged(nameEntryTextField)
+        nameSuggestionTableView.hidden = true
+        
         contactService.retrieveAllContacts() {
             contactListResult in
             
@@ -32,19 +37,79 @@ public class SingleNameEntryController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override public func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        nameEntryTextField.becomeFirstResponder()
+    }
+    
 
     // MARK: - Navigation
 
     override public func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if let spotifySongSelectionTableController = segue.destinationViewController as? SpotifySongSelectionTableController {
-            spotifySongSelectionTableController.searchContact = contact
+            spotifySongSelectionTableController.searchContact = selectedContact
             spotifySongSelectionTableController.songSelectionCompletionHandler = songSelectionCompletionHandler
         }
     }
     
+    public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return suggestedContacts.count
+    }
+    
+    public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("NameSuggestionTableCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        let contact = suggestedContacts[indexPath.row]
+        cell.textLabel?.text = contact.fullName
+
+        return cell
+    }
+    
+    public func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        selectedContact = suggestedContacts[indexPath.row]
+        nameSuggestionTableView.hidden = true
+        setNameEntryTextAndLastNameLabelForSelectedContact()
+    }
+    
+    func setNameEntryTextAndLastNameLabelForSelectedContact() {
+        let firstName = selectedContact!.firstName ?? ""
+        let lastName = selectedContact!.lastName ?? ""
+        nameEntryTextField.text = firstName
+        if firstName.isEmpty {
+            nameEntryTextField.text = lastName
+            lastNameLabel.text = ""
+        } else {
+            lastNameLabel.text = lastName.isEmpty ? "" : "(\(lastName))"
+        }
+    }
+    
     @IBAction public func nameEntryTextChanged(sender: UITextField) {
-        doneButton.enabled = !nameEntryTextField.text
-            .stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet()).isEmpty
+        let trimmedText = nameEntryTextField.text
+            .stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+        doneButton.enabled = !trimmedText.isEmpty
+        
+        selectedContact = nil
+        lastNameLabel.text = ""
+        
+        reloadNameSuggestionTableViewForText(trimmedText)
+    }
+    
+    func reloadNameSuggestionTableViewForText(text: String) {
+        suggestedContacts.removeAll(keepCapacity: false)
+        if !text.isEmpty {
+            for contact in allConctacts {
+                let contactWords = (split(contact.fullName) { $0 == " " })
+                    .map({ $0.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet()) })
+                let matchingWords = contactWords.filter({ $0.lowercaseString.hasPrefix(text) })
+                if !matchingWords.isEmpty {
+                    suggestedContacts.append(contact)
+                }
+            }
+        }
+        
+        suggestedContacts.sort({ $0.fullName < $1.fullName })
+        nameSuggestionTableView.hidden = suggestedContacts.isEmpty
+        nameSuggestionTableView.reloadData()
     }
     
     @IBAction public func cancelPressed(sender: UIBarButtonItem) {
@@ -52,10 +117,11 @@ public class SingleNameEntryController: UIViewController {
     }
     
     @IBAction public func donePressed(sender: UIBarButtonItem) {
-        contact = Contact(
-            id: -1,
-            firstName: nameEntryTextField.text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet()),
-            lastName: nil)
+        if selectedContact == nil {
+            selectedContact = Contact(id: -1,
+                firstName: nameEntryTextField.text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet()),
+                lastName: nil)
+        }
         performSegueWithIdentifier("SelectSongDifferentContactSegue", sender: sender)
     }
 }
--- TuneThatNameTests/Controllers/SingleNameEntryControllerSpec.swift
+++ TuneThatNameTests/Controllers/SingleNameEntryControllerSpec.swift
@@ -13,7 +13,9 @@ class SingleNameEntryControllerSpec: QuickSpec {
             var contactList = [
                 Contact(id: 1, firstName: "Johnny", lastName: "Bench", fullName: "Johnny Bench"),
                 Contact(id: 2, firstName: "Billy", lastName: "Idol", fullName: "Billy Idol"),
-                Contact(id: 3, firstName: "Frankie", lastName: "Avalon", fullName: "Frankie Avalon")
+                Contact(id: 3, firstName: "Frankie", lastName: "Avalon", fullName: "Frankie Avalon"),
+                Contact(id: 4, firstName: "Prince", lastName: nil, fullName: "Prince"),
+                Contact(id: 5, firstName: "", lastName: "Fletch", fullName: "Fletch")
             ]
             let songSelectionCompletionHandler = {
                 (song: Song, contact: Contact?) -> () in
@@ -44,6 +46,10 @@ class SingleNameEntryControllerSpec: QuickSpec {
                 it("disables the done button") {
                     expect(singleNameEntryController.doneButton.enabled).to(beFalse())
                 }
+                
+                it("hides the name suggestion table") {
+                    expect(singleNameEntryController.nameSuggestionTableView.hidden).to(beTrue())
+                }
             }
             
             describe("name entry text change") {
@@ -68,21 +74,146 @@ class SingleNameEntryControllerSpec: QuickSpec {
                 }
                 
                 context("when the text changes to non-empty") {
-                    let firstName = "Horatio"
                     beforeEach() {
                         self.changeNameEntryTextTo("", on: singleNameEntryController)
+                    }
+                    context("and the text does not start with the same characters as a word in one of the suggested full names") {
+                        let text = "c"
                         
-                        self.changeNameEntryTextTo(firstName, on: singleNameEntryController)
+                        beforeEach() {
+                            self.changeNameEntryTextTo(text, on: singleNameEntryController)
+                        }
+                        
+                        it("enables the done button") {
+                            expect(singleNameEntryController.doneButton.enabled).to(beTrue())
+                        }
+                        
+                        it("sets the first name on the contact") {
+                            self.pressDoneButton(singleNameEntryController)
+                            let spotifySongSelectionTableController = navigationController.topViewController as? SpotifySongSelectionTableController
+                            expect(spotifySongSelectionTableController?.searchContact.firstName).to(equal(text))
+                        }
+                        
+                        it("does not display the name suggestion table") {
+                            expect(singleNameEntryController.nameSuggestionTableView.hidden).to(beTrue())
+                        }
                     }
                     
-                    it("enables the done button") {
-                        expect(singleNameEntryController.doneButton.enabled).to(beTrue())
+                    context("and the text starts with the same characters as a word in one of the suggested full names") {
+                        let text = "b"
+                        let expectedSuggestedContacts = [contactList[1], contactList[0]]
+                        
+                        beforeEach() {
+                            self.changeNameEntryTextTo(text, on: singleNameEntryController)
+                        }
+                        
+                        it("enables the done button") {
+                            expect(singleNameEntryController.doneButton.enabled).to(beTrue())
+                        }
+                        
+                        it("displays the name suggestion table with the expectecd contacts") {
+                            expect(singleNameEntryController.nameSuggestionTableView.hidden).to(beFalse())
+                            expect(singleNameEntryController.tableView(singleNameEntryController.nameSuggestionTableView, numberOfRowsInSection: 0)).to(equal(2))
+                            expect(singleNameEntryController.tableView(
+                                singleNameEntryController.nameSuggestionTableView,
+                                cellForRowAtIndexPath: NSIndexPath(forRow: 0, inSection: 0)).textLabel?.text)
+                                .to(equal(expectedSuggestedContacts[0].fullName))
+                            expect(singleNameEntryController.tableView(
+                                singleNameEntryController.nameSuggestionTableView,
+                                cellForRowAtIndexPath: NSIndexPath(forRow: 1, inSection: 0)).textLabel?.text)
+                                .to(equal(expectedSuggestedContacts[1].fullName))
+                        }
+                    }
+                }
+            }
+            
+            describe("select a suggested name row") {
+                context("when the selected contact has a first and last name") {
+                    let text = "b"
+                    let expectedContact = contactList[0]
+                    
+                    beforeEach() {
+                        self.changeNameEntryTextTo(text, on: singleNameEntryController)
+                        
+                        singleNameEntryController.tableView(singleNameEntryController.nameSuggestionTableView, didSelectRowAtIndexPath: NSIndexPath(forRow: 1, inSection: 0))
                     }
                     
-                    it("sets the first name on the contact") {
+                    it("hides the name suggestion table") {
+                        expect(singleNameEntryController.nameSuggestionTableView.hidden).to(beTrue())
+                    }
+                    
+                    it("sets the name entry text field the expected value") {
+                        expect(singleNameEntryController.nameEntryTextField.text)
+                            .to(equal(expectedContact.firstName))
+                    }
+                    
+                    it("sets the last name label to the expected value") {
+                        expect(singleNameEntryController.lastNameLabel.text).to(equal("(\(expectedContact.lastName!))"))
+                    }
+                    
+                    it("sets the selected contact") {
                         self.pressDoneButton(singleNameEntryController)
                         let spotifySongSelectionTableController = navigationController.topViewController as? SpotifySongSelectionTableController
-                        expect(spotifySongSelectionTableController?.searchContact.firstName).to(equal(firstName))
+                        expect(spotifySongSelectionTableController?.searchContact).to(equal(expectedContact))
+                    }
+                    
+                    context("and the name entry text changes again") {
+                        let newText = expectedContact.fullName + "warmer"
+                        
+                        beforeEach() {
+                            self.changeNameEntryTextTo(newText, on: singleNameEntryController)
+                        }
+                        
+                        it("clears the last name label") {
+                            expect(singleNameEntryController.lastNameLabel.text).to(equal(""))
+                        }
+                        
+                        it("resets the selected contact") {
+                            self.pressDoneButton(singleNameEntryController)
+                            let spotifySongSelectionTableController = navigationController.topViewController as? SpotifySongSelectionTableController
+                            expect(spotifySongSelectionTableController?.searchContact.firstName).to(equal(newText))
+                        }
+                    }
+                }
+                
+                context("when the selected contact has no last name") {
+                    beforeEach() {
+                        self.changeNameEntryTextTo("pri", on: singleNameEntryController)
+                        
+                        singleNameEntryController.tableView(singleNameEntryController.nameSuggestionTableView, didSelectRowAtIndexPath: NSIndexPath(forRow: 0, inSection: 0))
+                    }
+                    
+                    it("clears the last name label") {
+                        expect(singleNameEntryController.lastNameLabel.text).to(equal(""))
+                    }
+                    
+                    it("sets the selected contact") {
+                        self.pressDoneButton(singleNameEntryController)
+                        let spotifySongSelectionTableController = navigationController.topViewController as? SpotifySongSelectionTableController
+                        expect(spotifySongSelectionTableController?.searchContact).to(equal(contactList[3]))
+                    }
+                }
+
+                context("when the selected contact has no first name") {
+                    beforeEach() {
+                        self.changeNameEntryTextTo("fle", on: singleNameEntryController)
+                        
+                        singleNameEntryController.tableView(singleNameEntryController.nameSuggestionTableView, didSelectRowAtIndexPath: NSIndexPath(forRow: 0, inSection: 0))
+                    }
+                    
+                    it("sets the name entry text field to the last name") {
+                        expect(singleNameEntryController.nameEntryTextField.text)
+                            .to(equal(contactList[4].lastName))
+                    }
+                    
+                    it("clears the last name label") {
+                        expect(singleNameEntryController.lastNameLabel.text).to(equal(""))
+                    }
+                    
+                    it("sets the selected contact") {
+                        self.pressDoneButton(singleNameEntryController)
+                        let spotifySongSelectionTableController = navigationController.topViewController as? SpotifySongSelectionTableController
+                        expect(spotifySongSelectionTableController?.searchContact).to(equal(contactList[4]))
                     }
                 }
             }
